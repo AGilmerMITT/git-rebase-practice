966e039a487135c9ccd1ab831184a812f9d3a283
Alex Gilmer <alex.gilmer@mitt.ca>
2023-11-09 14:47:00 -0600

    Type out initial possum-summon text

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("This happened");
+console.log("Invoking the summon-alex ritual");
+
+console.log("Behold, for I am the possum that is awesome...");
+console.log("Something something playing dead");
